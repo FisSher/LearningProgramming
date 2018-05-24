a40e075824aca297097f995c83d6cd4347fcf96e
FisSher <39218134+FisSher@users.noreply.github.com>
2018-05-24 16:25:19 -0300

    Add files via upload

--- CalculadoraIMC.py
+++ CalculadoraIMC.py
@@ -11,56 +11,41 @@ menos de 16.00 = delgadez severa infrapeso
 35 a 40 = obeso tipo 2
 mas de 40 = obeso tipo 3"""
 
-def calculoIMC():
+def pedirDatos():
     while True:
         try:
-            peso = float(input("Ingrese peso(en kilos): "))
-            altura = float (input("Ingrese altura (en metros): "))
-            if (peso<=0) | (altura <= 0):
-                raise ValueError ('No ha utilizado un número válido para el cálculo')
-            break
-        except ZeroDivisionError:
-            print ("Número inválido, división por cero.")
-        except ValueError:
-            print ("No ha utilizado un número válido para el cálculo")
-        except TypeError:
-            print("Valor inválido")
+            peso = float(input("Ingrese su peso en kilos: "))
+            altura = float(input("Ingrese su altura en metros: "))
+        except:
+            print("Debe ser un número")
+        else:
+            print (peso)
+            print (altura)
+            return False, peso, altura
 
-
-
-
+def calculoIMC(peso, altura):
     resultado = round(peso/(altura**2),2)
-    print ('\nSu índice de masa corporal es: ' + str(resultado))
-    if (resultado<16.00):
-        print("Usted posee delgadez severa o infrapeso")
-    elif(resultado >= 16.00) & (resultado <= 16.99):
-        print("Usted posee delgadez moderada")
-    elif(resultado >= 17.00) & (resultado <= 18.49):
-        print("Usted posee delgadez aceptable")
-    elif(resultado >= 18.50) & (resultado <= 24.99):
-        print("Usted posee un IMC normal")
-    elif(resultado >= 25.00) & (resultado <= 29.99):
-        print("Usted posee sobrepeso")
-    elif(resultado >= 30.00) & (resultado <= 49.99):
-        print("Usted posee obesidad tipo 1")
-    elif(resultado >= 35.00) & (resultado <= 40.00):
-        print("Usted posee obesidad tipo 2")
-    elif(resultado >= 40.00):
-        print("Usted posee obesidad tipo 3")
+    if (resultado<0):
+        print("No se admiten números negativos")
+    elif(resultado < 16.00):
+        print("Usted tiene delgadez severa o infrapeso")
+    elif (resultado >= 16.00) & (resultado <= 16.99):
+         print("Usted tiene delgadez moderada")
+    elif (resultado >=17.00) & (resultado <=18.49):
+        print("Usted tiene delgadez aceptable")
+    elif (resultado >=18.50) & (resultado <=24.99):
+        print("Usted tiene un IMC normal")
+    elif (resultado >=25.00) & (resultado <=29.99):
+        print("Usted tiene sobrepeso")
+    elif (resultado >=30.00) & (resultado <=34.99):
+        print("Usted tiene obesidad tipo 1")
+    elif (resultado >=35.00) & (resultado <=40.00):
+        print("Usted tiene obesidad tipo 2")
+    elif (resultado >40.00):
+        print("Usted tiene obesidad tipo 3")
     else:
-        print("Se produjo un error de datos")
-    print("\nGracias por utilizar la calculadora de IMC")
-
-def redo():
-    ans = None
-    ans = str.upper(input("¿Volver a calcular?(S para SI/ N para NO: "))
-    if (ans == 'S'):
-        calculoIMC()
-    elif(ans=='N'):
-        print("Hasta luego!")
-    else:
-        print("No es una opción válida")
+        print("No introdujo valores válidos")
 
 
-calculoIMC()
-redo()
+pedirDatos()
+
