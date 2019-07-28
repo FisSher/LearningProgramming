76f7910c4bf857d5e5df3e78dd228709403d6ff2
FisSher <39218134+FisSher@users.noreply.github.com>
2019-07-28 13:50:47 -0300

    Add files via upload

---
+++ Python/AutomateBoringStuff/1Introduccion.py
@@ -0,0 +1,12 @@
+nombre = input("Escribe tu nombre: ")
+print("Hola " + nombre)
+#Consultar la cantidad de letras del nombre usando len()
+print("La cantidad de letras del nombre es: ")
+print(len(nombre))
+#------------------------------------------------------------------------
+#Preguntar y escribir la edad
+edad=input("Ingresa tu edad: ")
+print("Tu edad es: " + edad)
+#Cuantos años voy a tener en un año?
+print("En un año vas a tener " + str(int(edad)+1) + " años.") 
+
---
+++ Python/AutomateBoringStuff/2FlujoControl.py
@@ -0,0 +1,8 @@
+name = input("Escriba nombre: ")
+age = int(input("Escriba la edad: "))
+if (name=="alice"):
+    print("Hola Alice")
+elif(age<12):
+    print("Vos no sos Alice")
+else:
+    print("No sos alice ni sos menor a 12")
