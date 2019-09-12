5be7a7f4dbd079d6684b674b44bff3037c966739
FisSher <39218134+FisSher@users.noreply.github.com>
2019-09-11 22:16:27 -0300

    Added infiltrated C# example for selenium
    I feel dirty but it has to be done.

---
+++ C#/CsharpExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Environment.SetEnvironmentVariable("geckodriver.exe", "D:\\Python");
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.com.ar");
+            /*driver.Manage().Window.Maximize();
+            IWebElement buscador = driver.FindElement(By.Name("q"));
+            buscador.SendKeys("C# y Selenium");
+            Console.WriteLine("20 vueltas para esto");
+            driver.Close();*/
+        }
+    }
+}
