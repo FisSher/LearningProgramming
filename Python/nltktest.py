54c111a6048f1799d52826be87d2cbfdf0cb2a47
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2019-11-17 17:25:04 -0300

    Creo nltktest

---
+++ Python/nltktest.py
@@ -0,0 +1,17 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def filtrarPalabras(tokens, stopwords):
+    for x in tokens: 
+        if x in stopwords: 
+            tokens.remove(x)
+    return tokens
+    
+
+plano = open(r'D:\Programming\Python\Cuento_Policial.txt','rb')
+texto = plano.read().decode('utf-8')
+tokens = [t for t in texto.split()]
+sr = stopwords.words('spanish')
+palabras_filtradas = filtrarPalabras(tokens, sr)
+#Trabajar con palabras_filtradas, el resto ya está hecho 
+#print(palabras_filtradas)
